d1433977352575fead46f9a78d48d74e9a7390b1
realkobe <56570273+realkobe@users.noreply.github.com>
2024-07-26 17:21:52 +0800

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1,7 +1,7 @@
 #nmcli c show
 ens=$(nmcli c show | awk 'NR==2{print $4}')
 #read -p "Enter ens:" ens
-echo $ens
+echo $ens--------------------
 #nmcli c edit $ens
 #goto ipv4
 read -p "Enter ip:" ip
@@ -22,23 +22,23 @@ sed -i 's/DNS1.*/DNS1=119.29.29.29/g' /etc/sysconfig/network-scripts/ifcfg-$ens
 sed -i 's/ONBOOT.*/ONBOOT=yes/g' /etc/sysconfig/network-scripts/ifcfg-$ens
 cat /etc/sysconfig/network-scripts/ifcfg-$ens
 
-echo ip add show
+echo ip add show--------------
 ip add show
-echo nmcli d reapply $ens
+echo nmcli d reapply $ens------------------
 nmcli d reapply $ens
-echo systemctl restart NetworkManager
+echo systemctl restart NetworkManager---------------------
 systemctl restart NetworkManager
 
-echo mount /dev/sr0 /mnt
+echo mount /dev/sr0 /mnt-----------------
 mount /dev/sr0 /mnt
-echo cd /etc/yum.repos.d/
+echo cd /etc/yum.repos.d/---------------------
 cd /etc/yum.repos.d/
-echo mkdir bak
+echo mkdir bak-----------------------
 mkdir bak
-echo mv openEuler.repo bak
+echo mv openEuler.repo bak-------------------------
 mv openEuler.repo bak
 
-
+echo touch openEuler.repo-----------------------
 touch openEuler.repo
 echo "[file]" >> openEuler.repo
 echo "name=iso" >> openEuler.repo
@@ -47,22 +47,22 @@ echo "enabled=1" >> openEuler.repo
 echo "gpgcheck=0" >> openEuler.repo
 cat openEuler.repo
 
-echo yum makecache
+echo yum makecache----------------------
 yum makecache
-echo yum install vim
+echo yum install vim------------------------
 yum install vim
-echo yum install httpd
+echo yum install httpd----------------------
 yum install httpd
 #systemctl status httpd
-echo systemctl start httpd
+echo systemctl start httpd----------------------
 systemctl start httpd
 #systemctl status httpd
-echo ss -antip
+echo ss -antip--------------------
 ss -antip
 
-echo systemctl status firewalld
+echo systemctl status firewalld--------------------------
 systemctl status firewalld
-echo firewall-cmd --permanent --add-port=80/tcp
+echo firewall-cmd --permanent --add-port=80/tcp----------------------------
 firewall-cmd --permanent --add-port=80/tcp
-echo firewall-cmd --reload
+echo firewall-cmd --reload------------------------------
 firewall-cmd --reload
