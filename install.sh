18df25fe2218bdc53d5d3db1b5c6d7e7b8b7a047
realkobe <56570273+realkobe@users.noreply.github.com>
2024-07-26 10:38:05 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,26 @@
+mount /dev/sr0 /mnt
+cd /etc/yum.repos.d/
+#mkdir bak
+#mv openEuler.repo bak
+
+touch openeuler2.repo
+echo "[file]" >> openeuler2.repo
+echo "name=iso" >> openeuler2.repo
+echo "baserurl=file:///mnt/" >> openeuler2.repo
+echo "enabled=1" >> openeuler2.repo
+echo "gpgcheck=0" >> openeuler2.repo
+
+
+yum makecache
+yum install vim
+y
+yum install httpd
+y
+#systemctl status httpd
+systemctl start httpd
+systemctl status httpd
+ss -antip
+
+systemctl status firewalld
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --reload
